01e7b71dca00074b6234fec555d02a8eae97982e
SANKALP1011 <tigerishan426@gmail.com>
2022-06-09 23:01:16 +0530

    ..working and fixing swagger docs.

--- api/Routes/fetchWeather.js
+++ api/Routes/fetchWeather.js
@@ -8,6 +8,29 @@ const express = require("express");
 const DbConnection = require("../Database/DatabaseConn");
 const routerthree = express.Router();
 
+/**
+ * @swagger
+ * /fetchWeather:
+ *   get:
+ *     summary: Retrieve the weather information for the particular city
+ *     description: Retieves cloud condition , temprature and one more variable from the weather api.
+ *     responses:
+ *       200:
+ *         description: Fetches the city searched and its weather from the database.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 weather:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                       description: City Name.
+ *                       example: Toronto
+*/
+
 routerthree.get("/fetchWeather",(req,res)=>{
     const FetchDataQuery = "Select* from WeatherData";
     DbConnection.query(FetchDataQuery,(err,result)=>{
